73aee6cc3e772d5401765c6a4cfef382436b796b
Agustin Santiago <119278931+weenstw@users.noreply.github.com>
2025-08-18 14:36:53 -0300

    fix de inicio de sesion login.js
    que guarde los datos de inicio de sesion en el localstorage

--- js/login.js
+++ js/login.js
@@ -4,10 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const usuario = document.getElementById('usuario').value.trim();    
+    const usuario = document.getElementById('usuario').value.trim();
     const contrasena = document.getElementById('contrasena').value.trim();
 
     if (usuario !== '' && contrasena !== '') {
+      localStorage.setItem('usuario', usuario);
+
       window.location.href = 'index.html';
     } else {
       alert('Por favor, complete ambos campos.');
